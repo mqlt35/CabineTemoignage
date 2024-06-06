728abc0a1363977e1b94e589c42b49e0d076792d
MDQ 01 Normal <MDQ 01 Normal@DESKTOP-E673PEP>
2024-06-06 18:54:11 +0200

    Ajoutez des fichiers projet.

---
+++ CabineTemoignage/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Device.Gpio;
+
+const int PinCombine = 21;
+
+using var controller = new GpioController();
+controller.OpenPin(PinCombine, PinMode.InputPullUp);
+
+Console.WriteLine(
+    $"Initialisation de la cabine de témoignage....");
+
+controller.RegisterCallbackForPinValueChangedEvent(
+    PinCombine,
+    PinEventTypes.Falling,
+    DecrochageCombine);
+
+controller.RegisterCallbackForPinValueChangedEvent(
+    PinCombine,
+    PinEventTypes.Rising,
+    RaccorchageCombine);
+
+
+Console.WriteLine(
+    $"La cabine de témoignage est disponible.");
+
+await Task.Delay(Timeout.Infinite);
+
+static void DecrochageCombine(object sender, PinValueChangedEventArgs args)
+{
+    Console.WriteLine("Decrochage Combiné");
+}
+
+static void RaccorchageCombine(object sender, PinValueChangedEventArgs args)
+{
+    Console.WriteLine("Raccrochage Combiné");
+}
