9dc093e432bbb12e79731a5cb87eca6ad3b01f69
MDQ La touche <contact@mqlt.fr>
2024-06-06 19:22:53 +0200

    feat : Initialisation arborescence état du programme

--- CabineTemoignage/Program.cs
+++ CabineTemoignage/Program.cs
@@ -1,35 +1,65 @@
 ﻿using System.Device.Gpio;
 
-const int PinCombine = 21;
-
-using var controller = new GpioController();
-controller.OpenPin(PinCombine, PinMode.InputPullUp);
-
-Console.WriteLine(
-    $"Initialisation de la cabine de témoignage....");
-
-controller.RegisterCallbackForPinValueChangedEvent(
-    PinCombine,
-    PinEventTypes.Falling,
-    DecrochageCombine);
-
-controller.RegisterCallbackForPinValueChangedEvent(
-    PinCombine,
-    PinEventTypes.Rising,
-    RaccorchageCombine);
-
-
-Console.WriteLine(
-    $"La cabine de témoignage est disponible.");
-
-await Task.Delay(Timeout.Infinite);
-
-static void DecrochageCombine(object sender, PinValueChangedEventArgs args)
+public static class Program
 {
-    Console.WriteLine("Decrochage Combiné");
-}
+    const int PinCombine = 21;
+    public static Etat etat { get; set; } = Etat.Initialisation;
 
-static void RaccorchageCombine(object sender, PinValueChangedEventArgs args)
-{
-    Console.WriteLine("Raccrochage Combiné");
+    public static async Task Main(string[] args) {
+        using var controller = new GpioController();
+        controller.OpenPin(PinCombine, PinMode.InputPullUp);
+
+        Console.WriteLine("Initialisation de la cabine de témoignage....");
+
+        controller.RegisterCallbackForPinValueChangedEvent(
+            PinCombine,
+            PinEventTypes.Falling,
+            DecrochageCombine);
+
+        controller.RegisterCallbackForPinValueChangedEvent(
+            PinCombine,
+            PinEventTypes.Rising,
+            RaccorchageCombine);
+
+        etat = Etat.Prete;
+
+        Console.WriteLine("La cabine de témoignage est disponible.");
+
+        await Task.Delay(Timeout.Infinite);
+    }
+
+    static void DecrochageCombine(object sender, PinValueChangedEventArgs args)
+    {
+        Console.WriteLine("Decrochage Combiné");
+        if (etat == Etat.Initialisation)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    static void RaccorchageCombine(object sender, PinValueChangedEventArgs args)
+    {
+        Console.WriteLine("Raccrochage Combiné");
+        if(etat == Etat.Initialisation || etat == Etat.Prete)
+            return;
+        if(etat == Etat.Enregistrement)
+        {
+            //TODO Arréter l'annonce 
+        }
+
+        //
+        etat = Etat.Prete;
+    }
+
+    public enum Etat
+    {
+        Initialisation,
+        Prete,
+        Annonce,
+        Enregistrement
+    }
 }
